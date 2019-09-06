c6a403c0d6712dac75d14bbc68e49e154c157122
hpraveen <hariharanster@gmail.com>
2019-09-06 00:16:54 -0400

    created db connection and serverJS file

---
+++ server.js
@@ -0,0 +1,29 @@
+
+const express = require("express"); // Server
+
+const exphbs = require("express-handlebars"); // Require Templating Engine Handlebars
+
+
+const app = express(); // Initalise Express Server
+
+let PORT = process.env.PORT || 8080; // Set Default Port for Express and Heroku
+app.use(express.urlencoded({ extended: false })); // Add Additional Functionality to Express Using Middleware body-parser
+app.use(express.static("public")); // Serve Static Content Such as CSS, JS, for the App from the "public" Directory in the Application Directory.
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+// Import routes and give the server access to them.
+// require("./controllers/burgersController.js")(app);
+
+// Equivalent???
+const routes = require("./controllers/burgersController.js");
+app.use("/", routes);
+
+
+/////////////////////////////////////////////// /* Start The Server */ //////////////////////////////////////////////////////////
+// Starts the server to begin listening
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
